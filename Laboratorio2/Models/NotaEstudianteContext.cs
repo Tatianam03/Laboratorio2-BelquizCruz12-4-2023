37a402543188e219b622d24d37e9812a3b83309d
Tatianam03 <cm0249042022@unad.edu.sv>
2023-04-12 11:02:41 -0600

    Programacion I

---
+++ Laboratorio2/Models/NotaEstudianteContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Laboratorio2.Models;
+
+public partial class NotaEstudianteContext : DbContext
+{
+    public NotaEstudianteContext()
+    {
+    }
+
+    public NotaEstudianteContext(DbContextOptions<NotaEstudianteContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Notum> Nota { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-NFDMETJ;Database=NotaEstudiante;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Notum>(entity =>
+        {
+            entity.HasKey(e => e.IdNotas).HasName("PK__Nota__858E853D3C97D6CC");
+
+            entity.Property(e => e.Lab1).HasColumnType("decimal(16, 2)");
+            entity.Property(e => e.Lab2).HasColumnType("decimal(16, 2)");
+            entity.Property(e => e.Lab3).HasColumnType("decimal(16, 2)");
+            entity.Property(e => e.NombreEstudiante)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreMateria)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Parcial1).HasColumnType("decimal(16, 2)");
+            entity.Property(e => e.Parcial2).HasColumnType("decimal(16, 2)");
+            entity.Property(e => e.Parcial3).HasColumnType("decimal(16, 2)");
+            entity.Property(e => e.Resultado).HasColumnType("decimal(16, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
